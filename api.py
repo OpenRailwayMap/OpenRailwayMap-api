2eadb63e60e8fdcfbe94bb1a6ed6f04bbd589fcd
Michael Reichert <michael.reichert@geofabrik.de>
2025-10-31 15:02:15 +0100

    Allow to set CORS header for local development use

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import argparse
 import psycopg2
 import psycopg2.extras
 import json
@@ -11,6 +12,8 @@ from werkzeug.wrappers import Request, Response
 from openrailwaymap_api.facility_api import FacilityAPI
 from openrailwaymap_api.milestone_api import MilestoneAPI
 
+cors = None
+
 def connect_db():
     conn = psycopg2.connect('dbname=gis')
     psycopg2.extras.register_hstore(conn)
@@ -20,11 +23,12 @@ class OpenRailwayMapAPI:
 
     db_conn = connect_db()
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.url_map = Map([
             Rule('/facility', endpoint=FacilityAPI, methods=('GET',)),
             Rule('/milestone', endpoint=MilestoneAPI, methods=('GET',)),
         ])
+        self.cors = kwargs.get('cors')
 
     def ensure_db_connection_alive(self):
         if self.db_conn.closed != 0:
@@ -46,6 +50,8 @@ class OpenRailwayMapAPI:
             if not response:
                 self.db_conn.close()
                 self.db_conn = connect_db()
+        if self.cors is not None:
+            response.headers['Access-Control-Allow-Origin'] = str(self.cors)
         return response
 
     def wsgi_app(self, environ, start_response):
@@ -55,16 +61,17 @@ class OpenRailwayMapAPI:
 
 
 def application(environ, start_response):
-    openrailwaymap_api = OpenRailwayMapAPI()
+    openrailwaymap_api = OpenRailwayMapAPI(cors=cors)
     response = openrailwaymap_api.dispatch_request(environ, start_response)
     return response(environ, start_response)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='OpenRailwayMap API development mode. Do not run this command for production but use an WSGI server instead.')
+    parser.add_argument('-c', '--cors', type=str, help='Value for HTTP header "Access-Control-Allow-Origin" if one should be set.')
+    parser.add_argument('-p', '--port', type=int, default=5000, help='HTTP port to listen at, defaults to 5000.')
+    args = parser.parse_args()
+    cors = args.cors
     openrailwaymap_api = OpenRailwayMapAPI()
-    if len(sys.argv) == 2 and sys.argv[1] == 'serve':
-        from werkzeug.serving import run_simple
-        run_simple('127.0.0.1', 5000, application, use_debugger=True, use_reloader=True)
-    else:
-        sys.stderr.write('ERROR: Missing argument \'serve\'. Cannot start in standalone development mode this way.\n')
-        exit(1)
+    from werkzeug.serving import run_simple
+    run_simple('127.0.0.1', args.port, application, use_debugger=True, use_reloader=True)
